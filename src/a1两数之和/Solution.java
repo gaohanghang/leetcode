1ade2369bdf81349a167848cfbb897befcbff1ae
gaohanghang <1341947277@qq.com>
2018-12-28 10:44:02 +0800

    Initial commit

---
+++ src/a1两数之和/Solution.java
@@ -0,0 +1,56 @@
+package a1两数之和;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description:
+ * @author: Gao Hang Hang
+ * @date 2018/12/26 10:07
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    // 方法一：暴力法 时间复杂度是o(n^2) 空间复杂度是o(1)
+    public static int[] towSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution"); // 例外异常
+    }
+
+    // 方法二：两遍哈希表
+    public static int[] towSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    // 方法三：一遍哈希表
+    public static int[] towSum3(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{ map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
