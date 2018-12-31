c3431ea09170e17b51e1beeda94dc73b9013d9b4
gaohanghang <1341947277@qq.com>
2018-12-31 19:41:53 +0800

    🖥 leetcode 14. 最长公共前缀

---
+++ src/a14_最长公共前缀/Solution.java
@@ -0,0 +1,67 @@
+package a14_最长公共前缀;
+
+/**
+ * @Description:
+ * @author: Gao Hang Hang
+ * @date 2018/12/31 17:29
+ */
+public class Solution {
+    public static void main(String[] args) {
+        String s = longestCommonPrefix(new String[]{"leets", "leetcode", "leet", "leeds"});
+        System.out.println(s);
+    }
+
+    // 方法一: 水平扫描
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() -1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+
+    // 方法二：垂直扫描
+    public String longestCommonPrefix2(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        for (int i = 0; i < strs[0].length() ; i++){
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j ++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c)
+                    return strs[0].substring(0, i);
+            }
+        }
+        return strs[0];
+    }
+
+
+
+    // 方法三：分而治之
+    public String longestCommonPrefix3(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        return longestCommonPrefix(strs, 0, strs.length - 1);
+    }
+
+    String longestCommonPrefix(String[] strs, int l, int r) {
+        if (l == r) {
+            return strs[l];
+        } else {
+            int mid = (l + r) / 2;
+            String lcpLeft = longestCommonPrefix(strs, l, mid);
+            String lcpRight = longestCommonPrefix(strs, mid + 1, r);
+            return commonPrefix(lcpLeft, lcpRight);
+        }
+    }
+
+    String commonPrefix(String left, String right) {
+        int min = Math.min(left.length(), right.length());
+        for (int i = 0; i < min; i++) {
+            if (left.charAt(i) != right.charAt(i))
+                return left.substring(0, i);
+        }
+        return left.substring(0, min);
+    }
+}
