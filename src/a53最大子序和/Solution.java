8c63fbaaa1504ced7cc023d5bae3073a9eb5bff2
gaohanghang <1341947277@qq.com>
2019-01-11 22:02:34 +0800

    🍎️ 算法

---
+++ src/a53最大子序和/Solution.java
@@ -0,0 +1,18 @@
+package a53最大子序和;
+
+/**
+ * @Description: 动态规划解法
+ * @author: Gao Hang Hang
+ * @date 2019/01/11 21:24
+ */
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int ans = nums[0]; // 最优解
+        int dp  = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp = Math.max(dp + nums[i], nums[i]);
+            ans = Math.max(ans, dp);
+        }
+        return ans;
+    }
+}
---
+++ src/a53最大子序和/Solution2.java
@@ -0,0 +1,30 @@
+package a53最大子序和;
+
+/**
+ * @Description: 分治法
+ * @author: Gao Hang Hang
+ * @date 2019/01/11 21:38
+ */
+public class Solution2 {
+    public int maxSubArray(int[] nums) {
+        return divide(nums,0,nums.length-1);
+    }
+    public int divide(int[] nums,int start,int end){
+        if(start>=end) return nums[start];//大于的情况就当是中间情况了
+        int mid=(start+end)/2;
+        int maxleft=divide(nums,start,mid-1);//左边最大和
+        int maxright=divide(nums,mid+1,end);
+        //计算包含中间的连续最大和
+        int midl=nums[mid];
+        for(int temp=mid-1,sum=nums[mid];temp>=start;temp--){//计算左边
+            sum+=nums[temp];
+            midl=midl>sum?midl:sum;
+        }
+        int midr=midl;
+        for(int temp=mid+1,sum=midr;temp<=end;temp++){//计算右边
+            sum+=nums[temp];
+            midr=midr>sum?midr:sum;
+        }
+        return Math.max(maxleft,Math.max(maxright,midr));//最大子串在左边，最大子串在右边，或者包含中间那个数
+    }
+}
