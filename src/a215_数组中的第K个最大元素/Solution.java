011d967fb350ae7e7c2222587f00bd8200d22693
gaohanghang <1341947277@qq.com>
2019-01-24 16:42:34 +0800

    <新增>(leetcode-215): leetcode数组中的第K个最大元素解法

---
+++ src/a215_数组中的第K个最大元素/Solution.java
@@ -0,0 +1,33 @@
+package a215_数组中的第K个最大元素;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/**
+ * @Description:
+ * @author: Gao Hang Hang
+ * @date 2019/01/24 09:54
+ */
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        // init heap 'the smallest element first' 初始化第一个元素最小的堆
+        PriorityQueue<Integer> heap = new PriorityQueue<>(); // 优先队列
+
+        // keep k largest elements in the heap
+        // 将第k大的元素保留在heap中
+        for (Integer n : nums) {
+            heap.add(n);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+
+        // output
+        return heap.poll();
+    }
+
+    public static void main(String[] args) {
+        int[] a = {3,2,1,5,6,4};
+        System.out.println(new Solution().findKthLargest(a, 2));
+    }
+}
