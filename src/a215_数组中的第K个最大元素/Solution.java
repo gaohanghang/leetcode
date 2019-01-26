484e7fb1d77aa8ba7385b7abd1f9585038fa33a3
gaohanghang <1341947277@qq.com>
2019-01-26 21:55:02 +0800

    <添加>(leetcode-215): 添加答案

--- src/a215_数组中的第K个最大元素/Solution.java
+++ src/a215_数组中的第K个最大元素/Solution.java
@@ -4,11 +4,17 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 /**
- * @Description:
+ * @Description: 使用堆
  * @author: Gao Hang Hang
  * @date 2019/01/24 09:54
  */
 public class Solution {
+    /*
+       Complexity Analysis
+           Time complexity : O(Nlog(k)).
+           pace complexity : O(k).
+    */
+
     public int findKthLargest(int[] nums, int k) {
         // init heap 'the smallest element first' 初始化第一个元素最小的堆
         PriorityQueue<Integer> heap = new PriorityQueue<>(); // 优先队列
--- src/a215_数组中的第K个最大元素/Solutuin2.java
+++ src/a215_数组中的第K个最大元素/Solutuin2.java
@@ -1,10 +1,34 @@
 package a215_数组中的第K个最大元素;
 
+import edu.princeton.cs.algs4.In;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 /**
  * @Description:
  * @author: Gao Hang Hang
  * @date 2019/01/26 16:41
  */
 public class Solutuin2 {
+    public static int findKthLargest(int[] nums, int k) {
+        // init heap 'the smallest element first'
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>((n1, n2) -> n1 - n2);
 
+        // keep k largest elements in the heap
+        for (Integer n : nums) {
+            heap.add(n);
+            if (heap.size() > k)
+                heap.poll();
+        }
+
+        // output
+        return heap.poll();
+    }
+
+    public static void main(String[] args) {
+        int[] a = {3, 2, 1, 5, 6, 4};
+        int kthLargest = findKthLargest(a, 2);
+        System.out.println(kthLargest);
+    }
 }
