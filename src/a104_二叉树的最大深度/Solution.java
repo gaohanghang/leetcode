00b3b938bd2c60c88f18664601011337cba86bbb
gaohanghang <1341947277@qq.com>
2019-04-20 13:17:15 +0800

    <添加>(leetcode): 添加答案

---
+++ src/a104_二叉树的最大深度/Solution.java
@@ -0,0 +1,14 @@
+package a104_二叉树的最大深度;
+
+// 递归
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left_height = maxDepth(root.left);
+            int right_height = maxDepth(root.right);
+            return Math.max(left_height, right_height) + 1;
+        }
+    }
+}
---
+++ src/a104_二叉树的最大深度/TreeNode.java
@@ -0,0 +1,11 @@
+package a104_二叉树的最大深度;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
