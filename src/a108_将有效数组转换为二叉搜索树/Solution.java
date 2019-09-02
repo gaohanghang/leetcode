f57499d15a1593c76f66f4ffc1629cf52b98f396
gaohanghang <1341947277@qq.com>
2019-09-02 21:26:19 +0800

    <添加>(leetcode): 添加答案

---
+++ src/a108_将有效数组转换为二叉搜索树/Solution.java
@@ -0,0 +1,30 @@
+package a108_将有效数组转换为二叉搜索树;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) return null;
+        TreeNode head = helper(nums, 0, nums.length - 1);
+        return head;
+    }
+
+    public TreeNode helper(int[] nums, int low, int high) {
+        // Done，边界条件
+        if (low > high) return null;
+        // 取中间值作为根节点
+        int mid = (low + high) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        // 使用递归方法构建左右子树
+        node.left = helper(nums, low, mid - 1);
+        node.right = helper(nums, mid + 1, high);
+        return node;
+    }
+}
---
+++ src/a108_将有效数组转换为二叉搜索树/TreeNode.java
@@ -0,0 +1,15 @@
+package a108_将有效数组转换为二叉搜索树;
+
+/**
+ * @Description
+ * @Author Gao Hang Hang
+ * @Date 2019-09-02 8:01 下午
+ **/
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
