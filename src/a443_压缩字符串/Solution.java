d8f54d703473b8b493909be9a1e015af1b962dbf
gaohanghang <1341947277@qq.com>
2019-09-17 01:05:11 +0800

    update

---
+++ src/a443_压缩字符串/Solution.java
@@ -0,0 +1,34 @@
+package a443_压缩字符串;
+
+/**
+ * @Description
+ * @Author Gao Hang Hang
+ * @Date 2019-09-16 23:48
+ **/
+public class Solution {
+    // 数组大小范围：1 <= chars.length <= 1000
+    public int compress(char[] chars) {
+        int left = 0;
+        int size = 0;
+
+        // 由于最后一个字符也需要判断，所以将右指针终点放到数组之外一格
+        for (int right = 0; right <= chars.length ; right++) {
+            // 当遍历完成，或右指针元素不等与左指针元素时，更新数组
+            if (right == chars.length || chars[right] != chars[left]) {
+                chars[size++] = chars[left]; // 更新字符
+                if (right - left > 1) {  // 更新计数，当个数大于 1 时才更新
+                    for (char c : String.valueOf(right - left).toCharArray()) {
+                        chars[size++] = c;
+                    }
+                }
+                left = right;
+            }
+        }
+        return size;
+    }
+
+    public static void main(String[] args) {
+        char[] chars = new char[]{'a','a','b','b','c','c','c'};
+        new Solution().compress(chars);
+    }
+}
