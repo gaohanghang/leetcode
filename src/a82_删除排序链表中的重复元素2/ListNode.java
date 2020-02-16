a7b02a74697c151d088cb10124074d94d7f8b4ee
gaohanghang <1341947277@qq.com>
2020-02-16 22:52:22 +0800

    update

---
+++ src/a82_删除排序链表中的重复元素2/ListNode.java
@@ -0,0 +1,16 @@
+package a82_删除排序链表中的重复元素2;
+
+import java.util.List;
+
+/**
+ * @Description
+ * @Author Gao Hang Hang
+ * @Date 2020-02-16 22:43
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/a82_删除排序链表中的重复元素2/Solution.java
@@ -0,0 +1,35 @@
+package a82_删除排序链表中的重复元素2;
+
+import java.util.List;
+
+/**
+ * @Description https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/discuss/28335/My-accepted-Java-code
+ * @Author Gao Hang Hang
+ * @Date 2020-02-16 22:44
+ **/
+public class Solution {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode FakeHead = new ListNode(0);
+        FakeHead.next = head;
+        ListNode pre = FakeHead;
+        ListNode cur = head;
+        while (cur != null) {
+            while (cur.next != null && cur.next.val) {
+                cur = cur.next;
+            }
+            if (pre.next == cur) {
+                pre = pre.next;
+            } else {
+                pre.next = cur.next;
+            }
+            // 指针向后移动一位
+            cur = cur.next;
+        }
+        return FakeHead.next;
+    }
+
+}
