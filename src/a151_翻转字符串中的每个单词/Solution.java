347c883da339adedd7af5efd27a08dc57e7184b5
gaohanghang <1341947277@qq.com>
2020-07-18 02:14:00 +0800

    update

---
+++ src/a151_翻转字符串中的每个单词/Solution.java
@@ -0,0 +1,21 @@
+package a151_翻转字符串中的每个单词;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 使用语言特性
+ */
+class Solution {
+
+    public String reverseWords(String s) {
+        // 除去开头和末尾的空白字符
+        s = s.trim();
+        // 正则匹配连续的空白字符作为分隔符分割
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+        Collections.reverse(wordList);
+        return String.join(" ", wordList);
+    }
+
+}
