d8591d8fa838786e93be87ab8daca9059e5a3b82
gaohanghang <1341947277@qq.com>
2020-08-06 23:42:18 +0800

    Default Changelist

--- src/a0算法面试题/单例设计模式/单例模式的七种写法2/a5/Singleton.java
+++ src/a0算法面试题/单例设计模式/单例模式的七种写法2/a5/Singleton.java
@@ -3,15 +3,15 @@ package a0算法面试题.单例设计模式.单例模式的七种写法2.a5;
 /**
  * @Description 5 静态内部类
  * @Author Gao Hang Hang
- * @Date 2020-08-06 10:41
+ * @Date 2020-08-06 23:37
  **/
 public class Singleton {
 
-    private Singleton(){
+    private static class SingletonHolder{
+        private static final Singleton INSTANCE = new Singleton();
     }
 
-    private static class SingletonHolder{
-        private static Singleton INSTANCE = new Singleton();
+    private Singleton() {
     }
 
     public static Singleton getInstance(){
