da9b68b54fa533d36c66444ac05040a3999e01a5
gaohanghang <1341947277@qq.com>
2020-09-24 22:49:47 +0800

    add

---
+++ src/a146_LRU缓存机制/LRUCache.java
@@ -0,0 +1,27 @@
+package a146_LRU缓存机制;
+
+class LRUCache {
+
+
+    public LRUCache(int capacity) {
+
+    }
+
+    // 找值，hashmap contains? 没有返回-1， 有就 update to head
+    public int get(int key) {
+        return 0;
+    }
+
+
+    //
+    public void put(int key, int value) {
+
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
---
+++ src/a146_LRU缓存机制/Solution.java
@@ -0,0 +1,9 @@
+package a146_LRU缓存机制;
+
+/**
+ * @Description
+ * @Author Gao Hang Hang
+ * @Date 2020-09-24 21:51
+ **/
+public class Solution {
+}
