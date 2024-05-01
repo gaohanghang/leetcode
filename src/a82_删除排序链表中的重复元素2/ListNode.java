37ce2d64b0d1bc539cffc92fd0c33c50a7f9b1c4
高行行 <1341947277@qq.com>
2024-05-01 23:52:03 +0800

    add

--- src/a82_删除排序链表中的重复元素2/ListNode.java
+++ src/a82_删除排序链表中的重复元素2/ListNode.java
@@ -10,7 +10,13 @@ import java.util.List;
 public class ListNode {
     int val;
     ListNode next;
+
     ListNode(int x) {
         val = x;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
--- src/a82_删除排序链表中的重复元素2/Solution.java
+++ src/a82_删除排序链表中的重复元素2/Solution.java
@@ -1,35 +1,25 @@
 package a82_删除排序链表中的重复元素2;
 
-import java.util.List;
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
 
-/**
- * @Description https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/discuss/28335/My-accepted-Java-code
- * @Author Gao Hang Hang
- * @Date 2020-02-16 22:44
- **/
-//public class Solution {
-//
-//    public ListNode deleteDuplicates(ListNode head) {
-//        if (head == null) {
-//            return null;
-//        }
-//        ListNode FakeHead = new ListNode(0);
-//        FakeHead.next = head;
-//        ListNode pre = FakeHead;
-//        ListNode cur = head;
-//        while (cur != null) {
-//            while (cur.next != null && cur.next.val) {
-//                cur = cur.next;
-//            }
-//            if (pre.next == cur) {
-//                pre = pre.next;
-//            } else {
-//                pre.next = cur.next;
-//            }
-//            // 指针向后移动一位
-//            cur = cur.next;
-//        }
-//        return FakeHead.next;
-//    }
-//
-//}
+        ListNode dummy = new ListNode(0, head);
+        ListNode cur = dummy;
+
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int x = cur.next.val;
+                while (cur.next != null && cur.next.val == x) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
