82877c4c495859c130b7b3c93756392621cb21b1
高行行 <1341947277@qq.com>
2024-08-23 18:10:15 +0800

    add

---
+++ src/a142_环形链表II/ListNode.java
@@ -0,0 +1,17 @@
+package a142_环形链表II;
+
+/**
+ * @author Gao Hanghang
+ * @version 1.0
+ * @description
+ * @date 2024-08-23 18:07
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
---
+++ src/a142_环形链表II/Solution.java
@@ -0,0 +1,35 @@
+package a142_环形链表II;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    // 3,2,0,-4
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) {
+                ListNode index1 = fast;
+                ListNode index2 = head;
+                while(index1 != index2) {
+                    index1 = index1.next;
+                    index2 = index2.next;
+                }
+                return index1;
+            }
+        }
+        return null;
+    }
+}
